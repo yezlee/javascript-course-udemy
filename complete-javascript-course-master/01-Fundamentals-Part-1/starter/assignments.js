66af0e5a9bf435587000dd9435e3d47fffc249fd
Yeseul Lee <dev.yezlee@gmail.com>
2021-05-18 23:51:02 +0900

    asgmt and Coding Challenge #4

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -3,52 +3,77 @@ const country = 'Korea';
 const continent = 'Asia';;
 let population = 52;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
 //LECTURE: Data Types
 const isIsland = false;
 let language;
 
-console.log(typeof isIsland, typeof population, typeof country, typeof language);
+// console.log(typeof isIsland, typeof population, typeof country, typeof language);
 
 //LECTURE: let, const and var
 language = 'Korean';
 //isIsland = true; //Uncaught TypeError: Assignment to constant variable.
 
 //LECTURE: Basic Operators
-console.log(population / 2);
+// console.log(population / 2);
 population++;
-console.log(population);
-console.log(population > 6);
-console.log(population < 33);
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
 
 const description = country + ' is in ' + continent + ', and its '+ population + ' million people speak ' + language;
 
-console.log(description);
+// console.log(description);
 
 //LECTRUE: Strings and Template Literals
 const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
-console.log(description2);
+// console.log(description2);
 
 //LECTURE: Taking Decisions: if / else Statements
-if(population > 33){
-    console.log("Korea's population is above average");
-}else{
-    console.log(`Korea's population is ${33 - population} million below average`);
-}
+// if(population > 33){
+//     console.log("Korea's population is above average");
+// }else{
+//     console.log(`Korea's population is ${33 - population} million below average`);
+// }
 
 //LECTURE: Type Conversion and Coercion
-console.log('9' - '5'); //4
-console.log('19' - '13' + '17'); //617
-console.log('19' - '13' + 17); //23
-console.log('123' < 57); //false
-console.log(5 + 6 + '4' + 9 - 4 - 2); //1143
-
+// console.log('9' - '5'); //4
+// console.log('19' - '13' + '17'); //617
+// console.log('19' - '13' + 17); //23
+// console.log('123' < 57); //false
+// console.log(5 + 6 + '4' + 9 - 4 - 2); //1143
 
 //LECTURE: Equality Operators: == vs. ===
-const numNeighbours = prompt('How many neighbour countries does your country have?');
 
+// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+// if(numNeighbours === 1){
+//     console.log('Only 1 border!')
+// }else if(numNeighbours > 1){
+//     console.log('More than 1 border.')
+// }else{
+//     console.log('No borders.')
+// }
+
+
+//LECTURE: Logical Operators
+// const speakEnglish = false;
+// const lessThan50MPeople = false;
+// const notAnIsland = true;
+
+// if(speakEnglish && lessThan50MPeople && notAnIsland){
+//     console.log('You should live in Korea :)')
+// }else{
+//     console.log('Korea does not meet your criteria.')
+// }
+
+// if(language === 'Korean' && population < 60 && !isIsland){
+//     console.log(`You should live in ${country} :)`)
+// }else{
+//     console.log(`${country} does not meet your criteria.`)
+// }
 
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -341,7 +341,6 @@ if (favourite === 23) { // 22 === 23 -> FALSE
 
 if (favourite !== 23) console.log('Why not 23?');
 
-*/
 
 ////////////////////////////////////
 // 23. Boolean Logic
@@ -367,6 +366,170 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log('Someone else should drive...');
 }
+*/
+
+////////////////////////////////////
+// Coding Challenge #3
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+// const averageDolphins = (96 + 108 + 89)/3;
+// const averageKoalas = (88 + 91 + 110)/3;
+
+// if(averageDolphins > averageKoalas){
+//     console.log(`Team Dolphins(${averageDolphins}) wins!`);
+// }else if(averageDolphins === averageKoalas){
+//     console.log('It is a draw.');
+// }else{
+//     console.log(`Team Koalas(${averageKoalas}) wins!`);
+// }
+
+// BONUS 1
+// const averageDolphins = (97 + 112 + 101)/3;
+// const averageKoalas = (109 + 95 + 123)/3;
+// console.log(averageKoalas, averageDolphins);
+
+// if(averageDolphins > averageKoalas && averageDolphins >= 100){
+//     console.log(`Team Dolphins(${averageDolphins}) wins!`);
+// }else if(averageDolphins < averageKoalas && averageKoalas >= 100){
+//     console.log(`Team Koalas(${averageKoalas}) wins!`);
+// }else if(averageDolphins === averageKoalas){
+//     console.log('It is a draw. Both win!');
+// }else{
+//     console.log('No one wins.')
+// }
+
+//BONUS 2
+// const averageDolphins = (97 + 112 + 11)/3;
+// const averageKoalas = (109 + 95 + 16)/3;
+// console.log(averageKoalas, averageDolphins);
+
+// if(averageDolphins > averageKoalas && averageDolphins >= 100){
+//     console.log(`Team Dolphins(${averageDolphins}) wins!`);
+// }else if(averageDolphins < averageKoalas && averageKoalas >= 100){
+//     console.log(`Team Koalas(${averageKoalas}) wins!`);
+// }else if(averageDolphins === averageKoalas && averageKoalas >= 100 && averageDolphins >= 100){
+//     console.log('It is a draw. Both win!');
+// }else if(averageDolphins === averageKoalas && averageKoalas >= 100 && averageDolphins >= 100){
+// }else{
+//     console.log('No one wins.')
+// }
+
+
+/*
+////////////////////////////////////
+// 26. The switch Statement
+const day = 'tuesday';
+
+switch(day) {
+    case 'monday': 
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D')
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if(day === 'monday'){
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+}else if(day === 'tuesday'){
+    console.log('Prepare theory videos');
+}else if(day === 'wednesday' || day === 'thursday'){
+    console.log('Write code examples');
+}else if(day === 'friday'){
+    console.log('Record videos');
+}else if(day === 'saturday' || day === 'sunday'){
+    console.log('Enjoy the weekend :D')
+}else{
+    console.log('Not a valid day!');
+}
+
+
+////////////////////////////////////
+// 27. Statements and Expressions
+3 + 4  //Expression
+1991   //Expression
+true && false && !false   //Expression
+
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+
+////////////////////////////////////
+// 28. The Conditional (Ternary) Operator
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
+
+*/
+
+
+////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+*/
+
+let tip;
+const bill = 500;
+const final = 50 <= bill && bill <= 300 ? (tip = bill*0.15) + bill : (tip = bill*0.2) + bill;
+
+console.log(final);
 
 
 
