9b288e95927ac3fd656415a442e33aaf4a43b742
Yeseul Lee <dev.yezlee@gmail.com>
2021-05-21 18:46:59 +0900

    Activating Strict Mode

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,27 @@
- 
+'use strict';
+
+/*
+///////////////////////////////////////
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+if(hasDriversLicense) console.log('I can drive :D');
+
+const interface = 'Audio';
+const private = 534;
+
+*/
+
+///////////////////////////////////////
+// Functions
+function logger(){
+    console.log('My name is Yeseul');
+}
+
+//calling / running / invoking function 
+logger();
+logger();
+logger();
+
