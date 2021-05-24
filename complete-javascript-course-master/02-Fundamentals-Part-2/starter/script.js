ec0347be17b9dbd254f80aa71ed2f6b3dfb1fc2e
Yeseul Lee <dev.yezlee@gmail.com>
2021-05-24 21:50:59 +0900

    Functions,  Function Declarations vs. Expressions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -12,10 +12,10 @@ if(hasDriversLicense) console.log('I can drive :D');
 const interface = 'Audio';
 const private = 534;
 
-*/
+
 
 ///////////////////////////////////////
-// Functions
+// 33. Functions
 function logger(){
     console.log('My name is Yeseul');
 }
@@ -25,3 +25,32 @@ logger();
 logger();
 logger();
 
+function fruitProcessor(apples, oranges){
+    const juice =  `Juice with ${apples} apples and ${oranges} oranges.`;
+    console.log(juice);
+    return juice;
+}
+
+console.log(fruitProcessor(5, 0));
+//or
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+*/
+
+///////////////////////////////////////
+// 34. Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYeah) {
+    return 2037 - birthYeah;
+}
+const age1 = calcAge1(1991);
+  
+// Function expression
+const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+const age2 = calcAge2(1991);
+  
+console.log(age1, age2);
+  
