0691e4a4a7ed9acde0762ad49e6e031c6accba7c
Yeseul Lee <dev.yezlee@gmail.com>
2021-05-25 21:47:15 +0900

    all functions - coding challange(still working)

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -35,7 +35,7 @@ console.log(fruitProcessor(5, 0));
 //or
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
-*/
+
 
 ///////////////////////////////////////
 // 34. Function Declarations vs. Expressions
@@ -53,4 +53,99 @@ const calcAge2 = function (birthYeah) {
 const age2 = calcAge2(1991);
   
 console.log(age1, age2);
-  
+*/
+
+///////////////////////////////////////
+// 35. Arrow functions
+
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3); //46
+
+const yearsUntilRetirement2 = birthYeah => {
+    const age = 2037 - birthYeah;
+    const retirement = 65 - age;
+    return retirement; 
+}
+console.log(yearsUntilRetirement2(1991)); //19
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas')); //Jonas retires in 19 years
+console.log(yearsUntilRetirement(1980, 'Bob')); //Bob retires in 8 years
+
+///////////////////////////////////////
+// 36. Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+  const age = calcAge(birthYeah);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+
+// const calcAverage = calcAverage(scoreDolphins1, scoreDolphins2, scoreDolphins3, ) 
+//     => (score1 + score2 + score3) / 3;
+
+
+// function checkWinner(avgDolphins, avgKoalas){
+
+//     console.log(`${winner} win (${winnerScore} vs. ${loserScore})`)
+// }
